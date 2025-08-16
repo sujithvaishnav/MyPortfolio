076dd8f7396b5eff531ee11ee8d1716b54726ae7
sujithvaishnav <sujithvaishnavmalla@gmail.com>
2025-08-16 23:48:26 +0530

    Update Projects.tsx

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -8,7 +8,7 @@ const Projects = () => {
     {
   title: "Hear The Image",
   description: "Developed an assistive application that converts images into descriptive audio outputs for visually impaired users.",
-  image: "https://plus.unsplash.com/premium_photo-1687880582685-b33bbca934b2?q=80&w=1040&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  image: "https://unsplash.com/photos/black-and-silver-dslr-camera-on-white-paper-PVi0X3WtevM",
   technologies: ["Python", "BLIP", "CV","gTTS", "Streamlit"],
   githubUrl: "https://github.com/sujithvaishnav/Hear_the_image",
   liveUrl: "https://imagetoaudio-89pedz2lydwockpbvrtivm.streamlit.app/",
